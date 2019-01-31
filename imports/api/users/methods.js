ee4e1a63dbfd21d524b6ea147a63490fd91ca7c2
Vadim Basko <vadim.p.basko@gmail.com>
2019-02-01 01:09:20 +0400

    list of users - allow api call only for admin roles

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -2,6 +2,12 @@ import {Meteor} from 'meteor/meteor';
 
 Meteor.methods({
   getUsers: function () {
+    let user = Meteor.user()
+    if (!Roles.userIsInRole(user, ['admin'])) {
+      console.warn(`user ${user._id} without admin privelege trying to get user list`)
+      return null
+    }
+
     let users = Meteor.users.find({}, {}).fetch()
     for (let user of users) {
       user.email = user.emails[0].address
