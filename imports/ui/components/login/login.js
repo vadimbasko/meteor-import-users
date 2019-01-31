8914b928ea0e873c6c09d97a79347a468b2eb5c6
Vadim Basko <vadim.p.basko@gmail.com>
2019-01-31 20:06:13 +0400

    login form

--- imports/ui/components/login/login.js
+++ imports/ui/components/login/login.js
@@ -1,1 +1,18 @@
 import './login.html'
+
+Template.login.events({
+
+  'submit form': function(event){
+    event.preventDefault();
+    let email = $('[name=email]').val();
+    let password = $('[name=password]').val();
+
+    Meteor.loginWithPassword(email, password, (err) => {
+      if (err) {
+        console.log(`Error login ${email}`, err)
+        alert(err.reason)
+      }
+    });
+  }
+
+});
